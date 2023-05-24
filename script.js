37603fcb1c2528d8ea037940a29541492d2d0845
HurricaneBB11 <107374463+HurricaneBB11@users.noreply.github.com>
2023-05-24 01:43:59 -0400

    Reset Tests

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     var loginBtn = document.getElementById('loginBtn');
     loginBtn.addEventListener('click', checkLoginPswd);
-var test = 9999
- this.hidden { var pass= ["hi"] }
+    
+
     function checkLoginPswd() {
         var validPasswords = ["wyattsfiles", "momspassword", "guestpassword"];
         var loginPassword = document.getElementById("loginPswd").value;
