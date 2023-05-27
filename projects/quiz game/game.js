4703f0477522bcab325b43e43df99dcc666d99a6
HurricaneBB11 <107374463+HurricaneBB11@users.noreply.github.com>
2023-05-27 18:40:34 -0400

    Added More Quetions And Bug Fixs

--- projects/quiz game/game.js
+++ projects/quiz game/game.js
@@ -1,23 +1,83 @@
 // Quiz data
 var questions = [
 	{
-	  question: "What is the capital of France?",
-	  options: ["Paris", "London", "Rome", "Madrid"],
-	  answer: 0
-	},
-	{
-	  question: "Which planet is known as the Red Planet?",
-	  options: ["Venus", "Mars", "Jupiter", "Saturn"],
-	  answer: 1
-	},
-	{
-	  question: "Who is dead?",
-	  options: ["Batman", "Mr. Clean", "Dobby", "The coder of this project"],
+	  question: "What was the first state in the US?",
+	  options: ["VA", "NY", "FL", "DE"],
 	  answer: 3
 	},
 	{
-	  question: "What is the largest ocean in the world?",
-	  options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+	  question: "In a basketball game, how many players are on the court at the same time?",
+	  options: ["8", "5", "7", "11"],
+	  answer: 1
+	},
+	{
+	  question: "What iconic company did Steve Jobs start?",
+	  options: ["Tasla", "Target", "Apple", "Wish.com"],
+	  answer: 2
+	},
+	{
+	  question: "What is the largest country in the world?",
+	  options: ["Canada", "Russia", "China", "USA"],
+	  answer: 1
+	},
+	{
+	  question: "What is the largest mammal?",
+	  options: ["Blue Whale", "Elephant", "Rhino", "Hippo"],
+	  answer: 1
+	},
+	{
+	  question: "What is the largest organ in the body?",
+	  options: ["Teeth", "Heart", "Brain", "Skin"],
+	  answer: 3
+	},
+	{
+	  question: "In what year was James Madison elected president of the US?",
+	  options: ["1796", "1784", "1924", "1809"],
+	  answer: 3
+	},
+	{
+	  question: "In what city can you find the Eiffel Tower?",
+	  options: ["London", "Paris", "New York", "Tokyo"],
+	  answer: 1
+	},
+	{
+	  question: "Name one of the Great Lakes",
+	  options: ["Lake Michigan", "Dead Sea", "Lake Winnipeg", "Red Lake"],
+	  answer: 1
+	},
+	{
+	  question: "What is the largest ocean?",
+	  options: ["Atlantic Ocean", "Arctic Ocean", "Southern Ocean", "Pacific Ocean"],
+	  answer: 3
+	},
+	{
+	  question: "What is the national bird of the US?",
+	  options: ["Bald Eagle", "Pigeon", "Peacock", "Falcon"],
+	  answer: 1
+	},
+	{
+	  question: "Who founded Amazon?",
+	  options: ["Steve Jobs", "Elon Musk", "Jeff Bezos", "Bill Gates"],
+	  answer: 2
+	},
+	{
+	  question: "Where was the game of checkers invented?",
+	  options: ["France", "USA", "Egypt", "Sudan"],
+	  answer: 3
+	},
+	{
+	  question: "Where did the Olympics start?",
+	  options: ["Greece", "Sudan", "USA", "Canada"],
+	  answer: 1
+	},
+	{
+	  question: "How many stars are in our solar system?",
+	  options: ["Infinity", "9B", "1", "65B"],
+	  answer: 2
+	},
+	{
+	  question: "How many time zones are there in the world?",
+	  options: ["32", "28", "19", "24"],
 	  answer: 3
 	},
 	// Add more questions...
@@ -123,13 +183,13 @@ var questions = [
   function endGame() {
 	var quizContainer = document.getElementById("quizContainer");
 	quizContainer.style.display = "none";
-	
+  
 	var scoreContainer = document.getElementById("scoreContainer");
 	var result = document.createElement("h2");
 	result.innerHTML = "Quiz Completed";
 	var scoreMsg = document.createElement("p");
 	scoreMsg.innerHTML = "Your score: " + score + " out of " + questions.length;
-	
+  
 	scoreContainer.appendChild(result);
 	scoreContainer.appendChild(scoreMsg);
 	scoreContainer.style.display = "block";
