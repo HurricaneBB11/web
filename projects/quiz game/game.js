41ff797bc8dcbd840350dd737ef84dabe5af3952
HurricaneBB11 <107374463+HurricaneBB11@users.noreply.github.com>
2023-06-03 23:01:25 -0400

    bug

--- projects/quiz game/game.js
+++ projects/quiz game/game.js
@@ -4,22 +4,22 @@ var questions = [
 	{
 	  question: "What was the first state in the US?",
 	  options: ["VA", "NY", "FL", "DE"],
-	  answer: 3
+	  answer: 4
 	},
 	{
 	  question: "In a basketball game, how many players are on the court at the same time?",
 	  options: ["8", "5", "7", "11"],
-	  answer: 1
+	  answer: 2
 	},
 	{
 	  question: "What iconic company did Steve Jobs start?",
 	  options: ["Tasla", "Target", "Apple", "Wish.com"],
-	  answer: 2
+	  answer: 3
 	},
 	{
 	  question: "What is the largest country in the world?",
 	  options: ["Canada", "Russia", "China", "USA"],
-	  answer: 1
+	  answer: 2
 	},
 	{
 	  question: "What is the largest mammal?",
@@ -29,17 +29,17 @@ var questions = [
 	{
 	  question: "What is the largest organ in the body?",
 	  options: ["Teeth", "Heart", "Brain", "Skin"],
-	  answer: 3
+	  answer: 4
 	},
 	{
 	  question: "In what year was James Madison elected president of the US?",
 	  options: ["1796", "1784", "1924", "1809"],
-	  answer: 3
+	  answer: 4
 	},
 	{
 	  question: "In what city can you find the Eiffel Tower?",
 	  options: ["London", "Paris", "New York", "Tokyo"],
-	  answer: 1
+	  answer: 2
 	},
 	{
 	  question: "Name one of the Great Lakes",
@@ -49,7 +49,7 @@ var questions = [
 	{
 	  question: "What is the largest ocean?",
 	  options: ["Atlantic Ocean", "Arctic Ocean", "Southern Ocean", "Pacific Ocean"],
-	  answer: 3
+	  answer: 4
 	},
 	{
 	  question: "What is the national bird of the US?",
@@ -59,12 +59,12 @@ var questions = [
 	{
 	  question: "Who founded Amazon?",
 	  options: ["Steve Jobs", "Elon Musk", "Jeff Bezos", "Bill Gates"],
-	  answer: 2
+	  answer: 3
 	},
 	{
 	  question: "Where was the game of checkers invented?",
 	  options: ["France", "USA", "Egypt", "Sudan"],
-	  answer: 3
+	  answer: 1
 	},
 	{
 	  question: "Where did the Olympics start?",
@@ -74,12 +74,12 @@ var questions = [
 	{
 	  question: "How many stars are in our solar system?",
 	  options: ["Infinity", "9B", "1", "65B"],
-	  answer: 2
+	  answer: 3
 	},
 	{
 	  question: "How many time zones are there in the world?",
 	  options: ["32", "28", "19", "24"],
-	  answer: 3
+	  answer: 4
 	},
 	// Add more questions...
   ];
